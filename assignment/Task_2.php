2fb8798d58ca4f8de5733d83f1e14aa1f3857042
Sabuz888 <122991925+Sabuz888@users.noreply.github.com>
2023-09-27 16:51:17 +0600

    update commit

--- assignment/Task_2.php
+++ assignment/Task_2.php
@@ -8,7 +8,7 @@
 ->to remove the even numbers from the array and print the resulting array.
 */ 
 
-$numbers = range(1, 10);
+$numbers = [1,2,3,4,5,6,7,8,9,10];
 
 function removeEven($numb) {
     $oddNumbers = [];
